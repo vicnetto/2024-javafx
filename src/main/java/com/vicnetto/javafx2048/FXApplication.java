7f1b94068de05ac86fbdc14d0e6399fd3cf5b2bb
Victor Netto <victorhmnetto@gmail.com>
2022-10-30 14:44:38 +0100

    :art: Updating FXApplication with new views and controllers

--- src/main/java/com/vicnetto/javafx2048/FXApplication.java
+++ src/main/java/com/vicnetto/javafx2048/FXApplication.java
@@ -1,23 +1,30 @@
 package com.vicnetto.javafx2048;
 
 import com.vicnetto.javafx2048.constant.WindowSize;
+import com.vicnetto.javafx2048.controller.BoardController;
 import com.vicnetto.javafx2048.controller.GameInformationController;
+import com.vicnetto.javafx2048.controller.MenuController;
+import com.vicnetto.javafx2048.model.Board;
+import com.vicnetto.javafx2048.view.BoardView;
 import com.vicnetto.javafx2048.view.GameInformationView;
 import com.vicnetto.javafx2048.view.MainView;
+import com.vicnetto.javafx2048.view.MenuView;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class FXApplication extends Application {
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) {
+        MenuView menuView = new MenuView();
+        BoardView boardView = new BoardView();
         GameInformationView gameInformationView = new GameInformationView();
-        MainView mainView = new MainView(null, null, gameInformationView);
+        MainView mainView = new MainView(menuView, boardView, gameInformationView);
 
         GameInformationController gameInformationController = new GameInformationController(gameInformationView);
+        BoardController boardController = new BoardController(boardView);
+        MenuController menuController = new MenuController(menuView, new Board(4), gameInformationController.getGameInformation());
 
         Scene scene = new Scene(mainView.getView(), WindowSize.WIDTH.getValue(), WindowSize.LENGTH.getValue());
         stage.setTitle("2024 - JavaFX");
